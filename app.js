ae9660d19e1f38b77484a7a945d7e982569d9027
Brian Young <Brian.Young@sacw.com>
2021-01-16 14:17:34 -0600

    progress on parsing data

--- app.js
+++ app.js
@@ -1,4 +1,44 @@
 
-d3.json("samples.json").then(function(data) {
-    console.log(data);
+
+function unpack(rows, index) {
+  return rows.map(function(row) {
+    return row[index];
   });
+}
+
+d3.json("samples.json").then(function (data) {
+  console.log(data);
+
+
+  let sample_values = data.samples;
+  let otu_ids = []
+  for (i = 0; i < data.samples.length; i++) {
+    otu_ids.push(data.samples[i].otu_ids);
+  };
+  // let otu_ids = unpack(data.samples, 1);
+  let otu_labels = data.samples.otu_lables;
+  let ids = data.names;
+
+  console.log(otu_ids);
+  console.log(ids);
+
+  // sample_values.sort(function compareFunction(firstNum, secondNum) {
+  //   return secondNum - firstNum;
+  // });
+  
+  console.log(sample_values);
+
+  // let trace1 = {
+  //   x: otu_ids,
+  //   y: sample_values,
+  //   type: "bar"
+  // };
+  // let bar_data = [trace1];
+  // let layout = {
+  //   orientation: "h"
+  // };
+
+  // Plotly.newplot("bar", bar_data, layout)
+
+
+});
