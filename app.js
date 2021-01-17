7a04855b2224a3fd03d292e111f68087fb8e8c17
Brian Young <Brian.Young@sacw.com>
2021-01-16 19:32:25 -0600

    commit 10

--- app.js
+++ app.js
@@ -13,19 +13,18 @@ d3.json("samples.json").then(function (data) {
     });
 
 
-    d3.select("#selDataset").on("change", optionChanged);
+  d3.select("#selDataset").on("change", optionChanged);
 
-    function optionChanged() {
-      let dropdown_id = d3.select("#selDataset").node();
-      // let dropdown_id = dropdown.value;
-      // update("onchange", this.value);
-    };
-    console.log(dropdown_id);
+  function optionChanged() {
+    let dropdown_id = d3.select("#selDataset").node().value;
+    // let dropdown_id = dropdown.value;
+    // update("onchange", this.value);
+    console.log(dropdown_id)
 
     let otu_ids = [];
     let sample_values = [];
     for (i = 0; i < data.samples.length; i++) {
-      if (data_id == data.samples[i].id) {
+      if (dropdown_id == data.samples[i].id) {
         let sliced_samples = data.samples[i].sample_values.slice(0, 10);
         let sliced_otu_ids = data.samples[i].otu_ids.slice(0, 10);
         otu_ids.push(sliced_otu_ids);
@@ -34,21 +33,21 @@ d3.json("samples.json").then(function (data) {
       };
     };
     console.log(otu_ids);
-    console.log(ids);
-
     console.log(sample_values);
 
-  });
 
-  let trace1 = {
-    x: otu_ids,
-    y: sample_values,
-    type: "bar",
-    orientation: "h"
+
+    let trace1 = {
+      x: otu_ids,
+      y: sample_values,
+      type: "bar",
+      orientation: "h"
+    };
+    let bar_data = [trace1];
+    let layout = {
+
+    };
+
+    Plotly.newplot("bar", bar_data, layout);
   };
-  let bar_data = [trace1];
-  let layout = {
-
-  };
-
-  Plotly.newplot("bar", bar_data, layout);
+});
